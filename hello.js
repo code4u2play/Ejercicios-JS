aaf2bba6cc4f32b4b4e4a4cf50cdb04f9141d0df
Charlie <wagiro@yahoo.com>
2017-10-06 21:41:49 -0500

    nuevo archivo

---
+++ hello.js
@@ -0,0 +1,10 @@
+ El volumen de la unidad C es OS
+ El n£mero de serie del volumen es: A869-36C0
+
+ Directorio de C:\Users\CharlieAdmin\Documents\Python centraal\GitPython\Ejercicios-JS
+
+06/10/2017  09:37 p. m.    <DIR>          .
+06/10/2017  09:37 p. m.    <DIR>          ..
+06/10/2017  09:37 p. m.                 0 hello.js
+               1 archivos              0 bytes
+               2 dirs  793,092,984,832 bytes libres
